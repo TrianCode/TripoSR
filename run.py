e35e956ddadb7b4d7b5941ad76f5062ca9a998e4
TrianCode <trianc03@gmail.com>
2025-08-06 20:44:13 +0800

    Update run.py

--- run.py
+++ run.py
@@ -53,7 +53,7 @@ parser.add_argument(
 )
 parser.add_argument(
     "--pretrained-model-name-or-path",
-    default="TrianC0de/TripoSR2",
+    default="TrianC0de/TripoSR",
     type=str,
     help="Path to the pretrained model. Could be either a huggingface model id is or a local path. Default: 'TrianC0de/TripoSR'",
 )
