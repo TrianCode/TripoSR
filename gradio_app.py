ae673751bd4be73881f559d2feb2344fd8f72492
TrianCode <trianc03@gmail.com>
2025-08-07 05:11:00 +0800

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -14,6 +14,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import json
 
+from tsr.system import TripoSR
 from tsr.system import TSR
 from tsr.utils import remove_background, resize_foreground, to_gradio_3d_orientation
 from tsr.evaluation import calculate_metrics
